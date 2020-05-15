5a110825bfaa76111cd00a46220a49890ebf2e94
OlgaTyulkina <61185779+OlgaTyulkina@users.noreply.github.com>
2020-05-15 16:44:08 +0300

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,25 +1,47 @@
 #pragma once
-#include<iostream>
-
+#include <fstream>
 using namespace std;
+
 class Complex
 {
-public:
-	Complex();
-	Complex(double x, double y);
-	Complex(Complex& A);
-	void set(double x, double y);
-	Complex get();
-	friend  Complex operator + (Complex &x, Complex &y);
-	friend Complex operator * (Complex &x, Complex &y);
-	friend Complex operator - (Complex &x, Complex &y);
-	friend Complex operator / (Complex &x, Complex &y);
-	Complex& operator = (const Complex &x);
-	friend bool operator == (Complex &x, Complex &y);
-	friend ostream& operator << (ostream &out, Complex &y);
-	friend istream& operator >> (istream &in, Complex &y);
-
 private:
-	double Re;
-	double Im;
+    double re, im;
+public:
+    void setre(double _re) { re = _re; }
+    double getre() { return re; }
+    void setim(double _im) { im = _im; }
+    double getim() { return im; }
+    void outPut(const char* name);
+
+    double& Re() { return re; }
+    double& Im() { return im; }
+ 
+    Complex();
+    Complex(const Complex& c);
+    Complex(double _re, double _im);
+    Complex(double _re);
+    ~Complex();
+
+ 
+    Complex operator+(const Complex& c2);
+    Complex operator+(double d);
+    friend Complex operator+(double d, const Complex& c2);
+    Complex operator-(const Complex& c2);
+    Complex& operator=(const Complex& C);
+    //Complex operator*(const Complex& c2);
+    Complex operator/(const Complex& c2);
+    bool operator==(const Complex& c2);
+    Complex operator++(); 
+    Complex operator++(int); 
+    Complex operator-();
+    Complex operator*(const Complex& c);
+    friend Complex operator*(double d, const Complex& c);
+    friend ostream& operator<<(ostream& stream, const Complex& c);
+    friend istream& operator>>(istream& stream, Complex& c);
+
+    
+    double module();
+    Complex exponen(int n);
+    Complex exponent(double z);
 };
+
